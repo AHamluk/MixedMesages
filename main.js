c68363afce162ac939d20728e0f9690a8325b37d
Aleksander <Aleksanderjhamluk@gmail.com>
2026-08-18 11:37:48 -0400

    * Added randomization for spell prefixes. * Removed unneeded array.

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // Project: Magical Astral Spell generator.
-console.log("Welcome to the Astral Spell Generator!");
+console.log("Welcome to the Astral Tome!");
 // git push origin master => Command to push commits to the remote repo. Reminder: init with a proper name next time.
-const spellName = [];
 const spellPrefix = ['Astral', 'Void', 'Stellar', 'Prism', 'Cosmic', 'Ether', 'Nebula', 'Stary'];
 const spellSuffix = ['Echo', 'Pulse', 'Shackle', 'of Doom', 'Bind', 'Blast'];
 // Create function to join spellnames and display message.
-const spellJoin = (prefix, name, suffix) => {return console.log(`You chant out one of the many magical tome's entries: ${prefix}: ${name} ${suffix}!`);}
+const spellJoin = (prefix, suffix) => {return console.log(`You chant out one of the many magical tome's entries: ${prefix} ${suffix}!`);}
+// Create variables for random indexes.
+let randSpellPrefix = spellPrefix[Math.floor(Math.random() * spellPrefix.length)]
