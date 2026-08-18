8af27d75692db0256189586791677df47f7467d2
Aleksander <Aleksanderjhamluk@gmail.com>
2026-08-18 11:23:43 -0400

    * Created arrays for Prefixes, Suffixes, and Names. * Added function to generate final message.

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 // Project: Magical Astral Spell generator.
 console.log("Welcome to the Astral Spell Generator!");
+// git push origin master => Command to push commits to the remote repo. Reminder: init with a proper name next time.
+const spellName = [];
+const spellPrefix = ['Astral', 'Void', 'Stellar', 'Prism', 'Cosmic', 'Ether', 'Nebula', 'Stary'];
+const spellSuffix = ['Echo', 'Pulse', 'Shackle', 'of Doom', 'Bind', 'Blast'];
+// Create function to join spellnames and display message.
+const spellJoin = (prefix, name, suffix) => {return console.log(`You chant out one of the many magical tome's entries: ${prefix}: ${name} ${suffix}!`);}
